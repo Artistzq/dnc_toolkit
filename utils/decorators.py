800803277b8bf44a3b64dd8e396fed782fb8dd26
Yao Zongqing <yaozongqing@outlook.com>
2023-05-07 22:33:33 +0800

    feat: add customized deprecated warning

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,4 +1,6 @@
 from functools import wraps
+import warnings
+
 
 def return_string(keys):
     def decorator(func):
@@ -12,4 +14,21 @@ def return_string(keys):
                 s += "{}: {}\t".format(key, r)
             return s
         return wrapped_function
+    return decorator
+
+
+def deprecated(reason=None, new=None):
+    def decorator(func):
+        @wraps(func)
+        def new_func(*args, **kwargs):
+            message = "Call to deprecated function {}.".format(func.__name__)
+            if reason:
+                message += "\n{}".format(reason)
+            if new:
+                message += "\nUse '{}' instead.".format(new)
+            warnings.warn(message,
+                          category=DeprecationWarning,
+                          stacklevel=2)
+            return func(*args, **kwargs)
+        return new_func
     return decorator
