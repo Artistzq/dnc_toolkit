3c09d758e33723df1a685efc612aff270e13f843
Yao Zongqing <yaozongqing@outlook.com>
2023-05-13 20:53:25 +0800

    feat: add api for merging dataset and dataloader

--- datasets/wrapper.py
+++ datasets/wrapper.py
@@ -1,6 +1,8 @@
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
+from torch.utils.data import ConcatDataset
 import torch
+from typing import List
 
 
 def tensor_to_dataset(images, labels):
@@ -13,7 +15,7 @@ def tensor_to_loader(images, labels, batch_size=128, shuffle=True, num_works=4):
 
 
 def dataset_to_tensor(dataset):
-    return torch.tensor(dataset.data)
+    return torch.tensor(dataset.data), torch.tensor(dataset.targets)
 
 
 def dataset_to_loader(dataset, batch_size=128, shuffle=True, num_works=4) -> DataLoader:
@@ -32,3 +34,16 @@ def loader_to_tensor(data_loader):
 def loader_to_dataset(data_loader) -> TensorDataset:
     X, y = loader_to_tensor(data_loader)
     return tensor_to_dataset(X, y)
+
+
+def merge_dataset(datasets):
+    return ConcatDataset(datasets)
+
+
+def merge_dataloader(data_loaders: List[DataLoader]):
+    batch_size = data_loaders[0].batch_size
+    num_works = data_loaders[0].num_workers
+    tensors = [loader_to_tensor(data_loader) for data_loader in data_loaders]
+    X = [tensor[0] for tensor in tensors]
+    y = [tensor[1] for tensor in tensors]
+    return tensor_to_loader(X, y, batch_size, False, num_works)
