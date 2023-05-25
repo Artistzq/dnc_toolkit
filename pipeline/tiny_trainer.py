e397eae2ea32f61232ef606ac5d993aced6b75ca
Yao Zongqing <yaozongqing@outlook.com>
2023-05-25 22:18:19 +0800

    refactor: refactor TinyTrainer

--- pipeline/tiny_trainer.py
+++ pipeline/tiny_trainer.py
@@ -1,105 +1,135 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
 
-from ..datasets import wrapper as data_wrapper
-from ..datasets.selector import Selector, UncertaintyBasedSelector
+
+# from ..utils.progress_bar import progress_bar
+from tqdm.auto import tqdm
+# from tqdm import tqdm_notebook as tqdm
 
 
 class TinyTrainer():
-  def __init__(self, model, device=None):
-    self.model = model
-    if not device:
-      self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else:
-      self.device = device
-    
-  def before_loss(self, loss, inputs, labels):
-    pass
-  
-  def after_inference(self):
-    pass
-  
-  def train(self, train_loader, val_loader, num_epochs, lr, criterion=None):
-    device = self.device
-    self.model = self.model.to(device)
-    
-    if not criterion:
-      criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=5e-4)
-    
-    for epoch in range(num_epochs):
-      train_loss = 0.0
-      val_loss = 0.0
-      train_correct = 0
-      train_total = 0
-      
-      self.model.train()
-      for i, (inputs, labels) in enumerate(train_loader):
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+    def __init__(self, model, device=None):
+        self.model = model
         
-        optimizer.zero_grad()
+        self.device = device
+        if not device:
+            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
-        outputs = self.model(inputs)
-        self.before_loss(criterion, inputs, labels)
         
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+
+    def before_loss(self, loss, inputs, labels):
+        pass
+
+    def after_inference(self):
+        pass
+
+    def train(self, train_loader, val_loader, num_epochs, lr, lr_scheduler=None, weight_decay=5e-4, criterion=None):
+        device = self.device
+        self.model = self.model.to(device)
+
+        if not criterion:
+            criterion = nn.CrossEntropyLoss()
+            
+        optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+
+        # milestones: [20%, 40%, 40%] [60, 120, 160]
+        milestones = [int(num_epochs * 0.3), int(num_epochs * 0.6)]
+        train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.5) #learning rate decay
         
-        train_loss += loss.item() * inputs.size(0)
-        _, predicted = torch.max(outputs.data, 1)
-        train_total += labels.size(0)
-        train_correct += (predicted == labels).sum().item()
-        
-      self.after_inference()
-      train_loss /= len(train_loader.dataset)
-      train_accuracy = train_correct / train_total
-      
-      self.model.eval()
-      val_correct = 0
-      val_total = 0
-      with torch.no_grad():
-        for i, (inputs, labels) in enumerate(val_loader):
-          inputs = inputs.to(device)
-          labels = labels.to(device)
-          
-          outputs = self.model(inputs)
-          
-          self.before_loss(criterion, inputs, labels)
-          loss = criterion(outputs, labels)
-          
-          val_loss += loss.item() * inputs.size(0)
-          _, predicted = torch.max(outputs.data, 1)
-          val_total += labels.size(0)
-          val_correct += (predicted == labels).sum().item()
-          
-      val_loss /= len(val_loader.dataset)
-      val_accuracy = val_correct / val_total
-      
-      print("Epoch {:>2} - Train Loss: {:.4f} - Val Loss: {:.4f} - Train Accuracy: {:.2f}% - Val Accuracy: {:.2f}%".format(epoch+1, train_loss, val_loss, train_accuracy*100, val_accuracy*100))
-  
-  def eval(self, test_loader):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    self.model = self.model.to(device)
+        for epoch in range(num_epochs):
+            
+            train_loss = 0.0
+            val_loss = 0.0
+            train_correct = 0
+            train_total = 0
+            
+            with tqdm(total=(len(train_loader))) as _tqdm:#总长度是data的长度
+                _tqdm.set_description(
+                    '[Training] Epoch [{:03}/{:03}] Lr [{:.1e}]'.format(epoch + 1, num_epochs, optimizer.param_groups[0]['lr'],)
+                )#前缀设置一些想要的更新信息
+                self.model.train()
+                for i, (inputs, labels) in enumerate(train_loader):
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-    correct = 0
-    total = 0
+                    optimizer.zero_grad()
 
-    self.model.eval()
-    with torch.no_grad():
-      for i, (inputs, labels) in enumerate(test_loader):
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+                    outputs = self.model(inputs)
+                    self.before_loss(criterion, inputs, labels)
 
-        outputs = self.model(inputs)
-        _, predicted = torch.max(outputs.data, 1)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
 
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+                    train_loss += loss.item() * inputs.size(0)
+                    _, predicted = torch.max(outputs.data, 1)
+                    train_total += labels.size(0)
+                    train_correct += (predicted == labels).sum().item()
 
-    accuracy = correct / total
-    print("Test Accuracy: {:.2f}%".format(accuracy * 100))
+                    _tqdm.set_postfix(
+                        Loss="{:.3f}".format(train_loss / train_total),
+                        Acc="{:.2f}%".format(100 * train_correct / train_total)
+                    )
+                    _tqdm.update(1)#更新步长为一个batchsize长度
+                    
+                self.after_inference()
 
+            self.model.eval()
+            val_correct = 0
+            val_total = 0
+            with torch.no_grad():
+                with tqdm(total=(len(val_loader))) as _tqdm:#总长度是data的长度
+                    _tqdm.set_description(
+                        '[Testing ] Epoch [{:03}/{:03}] Lr [{:.1e}]'.format(epoch + 1, num_epochs, optimizer.param_groups[0]['lr'],)
+                    )#前缀设置一些想要的更新信息
+                    for i, (inputs, labels) in enumerate(val_loader):
+                        inputs = inputs.to(device)
+                        labels = labels.to(device)
+
+                        outputs = self.model(inputs)
+
+                        self.before_loss(criterion, inputs, labels)
+                        loss = criterion(outputs, labels)
+
+                        val_loss += loss.item() * inputs.size(0)
+                        _, predicted = torch.max(outputs.data, 1)
+                        val_total += labels.size(0)
+                        val_correct += (predicted == labels).sum().item()
+
+                        _tqdm.set_postfix(
+                            Loss="{:.3f}".format(val_loss / val_total),
+                            Acc="{:.2f}%".format(100 * val_correct / val_total)
+                        )
+                        _tqdm.update(1)
+
+            # train_loss /= len(train_loader.dataset)
+            # train_accuracy = train_correct / train_total
+            # val_loss /= len(val_loader.dataset)
+            # val_accuracy = val_correct / val_total
+            # print("Epoch {:>2} - Train Loss: {:.4f} - Val Loss: {:.4f} - Train Accuracy: {:.2f}% - Val Accuracy: {:.2f}%".format(
+            #     epoch+1, train_loss, val_loss, train_accuracy*100, val_accuracy*100))
+            train_scheduler.step()
+            print()
+
+    def eval(self, test_loader):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = self.model.to(device)
+
+        correct = 0
+        total = 0
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (inputs, labels) in enumerate(test_loader):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outputs = self.model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = correct / total
+        print("Test Accuracy: {:.2f}%".format(accuracy * 100))
--- pipeline/trainer.py
+++ pipeline/trainer.py
@@ -1,89 +1,171 @@
+import os
+import argparse
+import json
+import random
+import time
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
 
-from ..datasets import wrapper as data_wrapper
-from ..datasets.selector import Selector, UncertaintyBasedSelector
+from torch.utils.data import DataLoader, Dataset
+from toolkit.datasets import wrapper as data_wrapper
+from toolkit.datasets.selector import Selector, UncertaintyBasedSelector
+
+from torch.utils.tensorboard import SummaryWriter
+
+from conf import settings
+from toolkit.utils.progress_bar import progress_bar
 
 
-class TinyTrainer():
-  def __init__(self, model):
-    self.model = model
-  
-  def train(self, train_loader, val_loader, num_epochs, lr):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    self.model = self.model.to(device)
+class Trainer:
     
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(self.model.parameters(), lr=lr, weight_decay=5e-4)
+    def __init__(self, 
+        net, 
+        criterion, 
+        device,  
+        batch_size,
+        optimizer, 
+        test_criterion=None,
+        tensorboard=None, 
+        ):
+        
+        self.net = net
+        self.device = device
+        self.loss_function = criterion
+        self.tensorboard = tensorboard
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+        self.test_criterion = test_criterion if test_criterion is not None else nn.CrossEntropyLoss()
+        if tensorboard:
+            self.__init_tensorboard()
+
     
-    for epoch in range(num_epochs):
-      train_loss = 0.0
-      val_loss = 0.0
-      train_correct = 0
-      train_total = 0
-      
-      self.model.train()
-      for i, (inputs, labels) in enumerate(train_loader):
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+    def __init_tensorboard(self):
+        self.writer = SummaryWriter(log_dir=os.path.join(settings.LOG_DIR, args.net, settings.TIME_NOW + "_" + args.add_info))
+        pass
         
-        optimizer.zero_grad()
+    def __loss_function_ops(self, images, labels):
+        pass
+    
+    def fit(self, model, training_loader, test_loader, epoch_nums):
+        pass
+    
+    def __train_single_epoch(self, epoch, training_loader, verbose=True):
+        net = self.net
+        optimizer = self.optimizer
+        writer = self.writer
         
-        outputs = self.model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+        start = time.time()
+        print('======> Training Network.....')
+        net.train()
         
-        train_loss += loss.item() * inputs.size(0)
-        _, predicted = torch.max(outputs.data, 1)
-        train_total += labels.size(0)
-        train_correct += (predicted == labels).sum().item()
+        correct = 0
+        total = 0
+        train_loss = 0
         
-      train_loss /= len(train_loader.dataset)
-      train_accuracy = train_correct / train_total
-      
-      self.model.eval()
-      val_correct = 0
-      val_total = 0
-      with torch.no_grad():
-        for i, (inputs, labels) in enumerate(val_loader):
-          inputs = inputs.to(device)
-          labels = labels.to(device)
-          
-          outputs = self.model(inputs)
-          loss = criterion(outputs, labels)
-          
-          val_loss += loss.item() * inputs.size(0)
-          _, predicted = torch.max(outputs.data, 1)
-          val_total += labels.size(0)
-          val_correct += (predicted == labels).sum().item()
-          
-      val_loss /= len(val_loader.dataset)
-      val_accuracy = val_correct / val_total
-      
-      print("Epoch {:>2} - Train Loss: {:.4f} - Val Loss: {:.4f} - Train Accuracy: {:.2f}% - Val Accuracy: {:.2f}%".format(epoch+1, train_loss, val_loss, train_accuracy*100, val_accuracy*100))
-  
-  def eval(self, test_loader):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    self.model = self.model.to(device)
+        for batch_index, (images, labels) in enumerate(training_loader):
+            
+            labels = labels.to(self.device)
+            images = images.to(self.device)
 
-    correct = 0
-    total = 0
+            # 预留的接口
+            self.__loss_function_ops(images, labels)
 
-    self.model.eval()
-    with torch.no_grad():
-      for i, (inputs, labels) in enumerate(test_loader):
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(images)
+            
+            # with torch.autograd.detect_anomaly():
+            loss = self.loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            # tensorboard
+            if self.tensorboard:
+                n_iter = (epoch - 1) * len(training_loader) + batch_index + 1
+                last_layer = list(net.children())[-1]
+                for name, para in last_layer.named_parameters():
+                    if 'weight' in name:
+                        writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
+                    if 'bias' in name:
+                        writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
+            
+            # 统计数据
+            train_loss += loss.item()
+            total += labels.size(0)
+            correct += outputs.max(1)[1].eq(labels).sum().item()
+            msg = 'Epoch: {:3} | Loss: {:0.4f} | ACC: {:0.2f} | LR: {:0.6f} | [{trained_samples}/{total_samples}]'.format(
+                epoch,
+                loss.item(),
+                100.0 * correct / total,
+                optimizer.param_groups[0]['lr'],
+                trained_samples=batch_index * self.batch_size + len(images),
+                total_samples=len(training_loader.dataset)
+            )
+            progress_bar(batch_index, len(training_loader), msg)
 
-        outputs = self.model(inputs)
-        _, predicted = torch.max(outputs.data, 1)
+            # tensorboard
+            if self.tensorboard:
+                writer.add_scalar('Train/loss', loss.item(), n_iter)
 
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+            if epoch <= args.warm:
+                warmup_scheduler.step()
 
-    accuracy = correct / total
-    print("Test Accuracy: {:.2f}%".format(accuracy * 100))
+        finish = time.time()
 
+        # tensorboard
+        if self.tensorboard:
+            writer.add_scalar('Train/Accuracy', correct /  total, epoch)
+            writer.add_scalar('Train/LearningRate', optimizer.param_groups[0]['lr'], epoch)
+        
+        print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
+        return 100.0 * correct / total, train_loss / len(training_loader.dataset)
+
+    @torch.no_grad()
+    def __test_single_epoch(self, epoch, test_loader):
+        self.net.eval()
+        print('======> Evaluating Network.....')
+        test_loss = 0.0
+        correct = 0.0
+        total = 0
+
+        for idx, (images, labels) in enumerate(test_loader):
+
+            labels = labels.to(self.device)
+            images = images.to(self.device)
+
+            outputs = self.net(images)
+            loss = self.test_criterion(outputs, labels)
+
+            test_loss += loss.item()
+            _, preds = outputs.max(1)
+            correct += preds.eq(labels).sum().item()
+            total += labels.size(0)
+            msg = 'Epoch: {:3} | Loss: {:.4f} | ACC: {:.2f}'.format(
+                epoch,
+                test_loss / len(test_loader.dataset),
+                100.0 * correct / total,
+            )
+            progress_bar(idx, len(test_loader), msg)
+        print()
+        
+        if self.tensorboard:
+            writer.add_scalar('Test/Average loss', test_loss / len(test_loader.dataset), epoch)
+            writer.add_scalar('Test/Accuracy', correct / len(test_loader.dataset), epoch)
+
+        return 100.0 * correct / len(test_loader.dataset), test_loss / len(test_loader.dataset)
+
+    def select_data(self, datasource, k, selector: Selector, as_loader=False):
+        if isinstance(datasource, DataLoader):
+            datasource = data_wapper.loader_to_dataset(datasource)
+        # datasource现在是个Dataset
+        indices = selector.select(datasource, k)
+        selected = torch.utils.data.Subset(datasource, indices)
+        if as_loader:
+            return data_wapper.dataset_to_loader(selected, self.batch_size, shuffle=False)
+        return selected
+
+
+if __name__ == '__main__':
+    pass
