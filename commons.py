fd8d2c8414e7fee0da0ff850247c1401db7b9268
Yao Zongqing <yaozongqing@outlook.com>
2023-06-09 17:28:06 +0800

    update common import

--- commons.py
+++ commons.py
@@ -1,11 +1,17 @@
+import io
+import os
+import sys
+import time
 import torch
-import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
 
 from matplotlib import pyplot as plt
 import seaborn as sns
 
 from .datasets import get_dataset, get_poisoned_dataset, get_corrupt_dataset, wrapper
+from .datasets import CIFAR10_MEAN_STD, CIFAR100_MEAN_STD, TINY_IMAGENET_MEAN_STD
 from .datasets.selector import UncertaintyBasedSelector
 from .datasets.cv_datasets import Dataset
 from .evaluate import ModelMetric, Uncertainty, evaluate_kit
@@ -15,8 +21,7 @@ from .loss import KnowledgeDistillationLoss
 from .pipeline import TinyTrainer as Trainer
 from .pipeline import Archive
 
-from .se4ai.disagreements import DiffChaser, SameChaser, Diffinder, SameFinder
-from .se4ai.disagreements import find_agreements, find_disagreements
+from .se4ai.disagreements import DiffChaser, SameChaser, Diffinder, SameFinder, CWDiffinder
 from .se4ai.compression import Pruner, TorchQuantizer
 
 from .utils.context_manager import Timer
